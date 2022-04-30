f4c30ac78a33bce49abd41c039969585383f785f
Jonne Välimäki <jonne.valimaki@tuni.fi>
2022-04-30 19:01:14 +0300

    Add project files.

---
+++ Koulu.h
@@ -0,0 +1,24 @@
+
+#pragma once
+#include <string>
+#include <vector>
+
+using std::string;
+
+class Koulu
+{
+public:
+
+	Koulu();
+	Koulu(string mKoulunNimi);
+	virtual void lisaaOpiskelija();
+
+private:
+
+	string mKoulunNimi = "xxxx";
+
+	//Miksi tämä hajoittaa ohjelman?
+	//vector <Opiskelija> mOpiskelijat;
+	//vector <Opettaja> mOpettajat;
+
+};
